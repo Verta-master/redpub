cd8e536ef2300c074c3a8f4f14853079fed2b23a
Verta-master <vertyagina@gmail.com>
2020-06-09 14:39:42 +0600

    Adaptive

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
-jQuery(function($) {
-	"use strict";
-
 //Mobile menu
-$('.menu__btn').click(function() {
-  $('.menu').toggleClass('menu--opened');
-  $('.menu__list').slideToggle();
-})
+$('.mobile__wrap').niceScroll();
 
-if (window.innerWidth < 766) {
-  $('.menu__link').click(function() {
-    $('.menu__list').slideToggle();
-    $('.menu').removeClass('menu--opened');
-  })
-}
+$('.menu__btn').click(function() {
+  $('.mobile').addClass('mobile--opened');
+});
+
+$('.mobile__close').click(function() {
+  $('.mobile').removeClass('mobile--opened');
+});
+
+$('.mobile__plus').click(function() {
+  $(this).toggleClass('mobile__plus--minus');
+  $(this).next().slideToggle();
+});
 
 //Scroll to top
 $(window).scroll(function(){
@@ -28,43 +28,4 @@ $(".btn-top").click(function () {
     scrollTop: 0
   }, 400);
   return false;
-});
-
-//Scroll to menu anchor
-$(document).ready(function () {
-  $(document).on("scroll", onScroll);
-
-  //smoothscroll
-  $('.menu__item a[href^="#"]').on('click', function (e) {
-    e.preventDefault();
-    $(document).off("scroll");
-
-    $('.menu__item a').each(function () {
-        $(this).removeClass('menu__link--active');
-    })
-    $(this).addClass('menu__link--active');
-
-    var target = this.hash,
-        menu = target;
-    $target = $(target);
-    $('html, body').stop().animate({
-        'scrollTop': $target.offset().top
-    }, 500, 'swing', function () {
-        $(document).on("scroll", onScroll);
-    });
-  });
-});
-
-function onScroll(event){
-  var scrollPos = $(document).scrollTop();
-  $('.menu__link').each(function () {
-      event.preventDefault();
-      var currLink = $(this);
-      var refElement = $(currLink.attr("href"));
-      if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-          $('.menu__link').removeClass("menu__link--active");
-          currLink.addClass("menu__link--active");
-      }
-  });
-};
 });
